339607fe6f3ddc2192b5e1189e2d47257efb100b
Abil Aubakirov <aubakirov.abil@gmail.com>
2020-05-01 15:53:27 +0200

    hello username

--- my_solution.py
+++ my_solution.py
@@ -1,9 +1,12 @@
 import sys
 
-dig_str = sys.argv[1]
+# dig_str = sys.argv[1]
+#
+# s = 0
+# for k in dig_str:
+#     s += int(k)
+#
+# print(str(s), end="")
 
-s = 0
-for k in dig_str:
-    s += int(k)
-
-print(str(s), end="")
+who = "world" if len(sys.argv)<2 else sys.argv[1]
+print("hello,  %s" % who)
